4e5696a55f54a70c3a207580bee87a8791776906
ccostoso <carlos.costoso@outlook.com>
2020-09-09 16:08:09 -0400

    Controller

---
+++ orderbook/src/main/java/com/sg/orderbook/controller/MainController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.orderbook.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author sealo
+ */
+@Controller
+public class MainController {
+    // index.html <-- with ticker
+    // bookSymbol.html <-- book with latest information for a given symbol, like in PowerPoint example
+    // history.html <-- history of all trades
+    // historyForSymbol <--history of trade for symbol
+    
+
+    // matchOrder() <-- either bring size of order to 0
+        // checks to see if it is possible to 
+    // deleteOrder() <-- deletes order from order table
+}
