3e88a5a1096a34fee9d0053d4d126322f8b21f47
Minul Haque <mhaque2016@gmail.com>
2020-09-09 23:20:45 -0400

    filled out entities

--- orderbook/src/main/java/com/sg/orderbook/entities/Order.java
+++ orderbook/src/main/java/com/sg/orderbook/entities/Order.java
@@ -7,6 +7,7 @@ package com.sg.orderbook.entities;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -42,5 +43,115 @@ public class Order {
     
     @Column
     private String symbol;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public boolean isSide() {
+        return side;
+    }
+
+    public void setSide(boolean side) {
+        this.side = side;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public BigDecimal getOfferPrice() {
+        return offerPrice;
+    }
+
+    public void setOfferPrice(BigDecimal offerPrice) {
+        this.offerPrice = offerPrice;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 11 * hash + this.id;
+        hash = 11 * hash + this.size;
+        hash = 11 * hash + (this.side ? 1 : 0);
+        hash = 11 * hash + Objects.hashCode(this.time);
+        hash = 11 * hash + (this.active ? 1 : 0);
+        hash = 11 * hash + Objects.hashCode(this.offerPrice);
+        hash = 11 * hash + Objects.hashCode(this.symbol);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Order other = (Order) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.size != other.size) {
+            return false;
+        }
+        if (this.side != other.side) {
+            return false;
+        }
+        if (this.active != other.active) {
+            return false;
+        }
+        if (!Objects.equals(this.symbol, other.symbol)) {
+            return false;
+        }
+        if (!Objects.equals(this.time, other.time)) {
+            return false;
+        }
+        if (!Objects.equals(this.offerPrice, other.offerPrice)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" + "id=" + id + ", size=" + size + ", side=" + side + ", time=" + time + ", active=" + active + ", offerPrice=" + offerPrice + ", symbol=" + symbol + '}';
+    }
     
 }
--- orderbook/src/main/java/com/sg/orderbook/entities/Transaction.java
+++ orderbook/src/main/java/com/sg/orderbook/entities/Transaction.java
@@ -5,7 +5,16 @@
  */
 package com.sg.orderbook.entities;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 /**
  *
@@ -14,5 +23,139 @@ import javax.persistence.Entity;
 @Entity
 public class Transaction {
     
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "transactionid")
     private int id;
+    
+    @Column
+    private LocalDateTime finalTime;
+    
+    @Column
+    private BigDecimal finalPrice;
+    
+    @Column
+    private int amount;
+    
+    @Column
+    private String finalSymbol;
+    
+    @ManyToOne
+    @JoinColumn(name = "orderid")
+    private Order buyOrder;
+    
+    @ManyToOne
+    @JoinColumn(name = "orderid")
+    private Order sellOrder;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getFinalTime() {
+        return finalTime;
+    }
+
+    public void setFinalTime(LocalDateTime finalTime) {
+        this.finalTime = finalTime;
+    }
+
+    public BigDecimal getFinalPrice() {
+        return finalPrice;
+    }
+
+    public void setFinalPrice(BigDecimal finalPrice) {
+        this.finalPrice = finalPrice;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getFinalSymbol() {
+        return finalSymbol;
+    }
+
+    public void setFinalSymbol(String finalSymbol) {
+        this.finalSymbol = finalSymbol;
+    }
+
+    public Order getBuyOrder() {
+        return buyOrder;
+    }
+
+    public void setBuyOrder(Order buyOrder) {
+        this.buyOrder = buyOrder;
+    }
+
+    public Order getSellOrder() {
+        return sellOrder;
+    }
+
+    public void setSellOrder(Order sellOrder) {
+        this.sellOrder = sellOrder;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 11 * hash + this.id;
+        hash = 11 * hash + Objects.hashCode(this.finalTime);
+        hash = 11 * hash + Objects.hashCode(this.finalPrice);
+        hash = 11 * hash + this.amount;
+        hash = 11 * hash + Objects.hashCode(this.finalSymbol);
+        hash = 11 * hash + Objects.hashCode(this.buyOrder);
+        hash = 11 * hash + Objects.hashCode(this.sellOrder);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Transaction other = (Transaction) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.amount != other.amount) {
+            return false;
+        }
+        if (!Objects.equals(this.finalSymbol, other.finalSymbol)) {
+            return false;
+        }
+        if (!Objects.equals(this.finalTime, other.finalTime)) {
+            return false;
+        }
+        if (!Objects.equals(this.finalPrice, other.finalPrice)) {
+            return false;
+        }
+        if (!Objects.equals(this.buyOrder, other.buyOrder)) {
+            return false;
+        }
+        if (!Objects.equals(this.sellOrder, other.sellOrder)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" + "id=" + id + ", finalTime=" + finalTime + ", finalPrice=" + finalPrice + ", amount=" + amount + ", finalSymbol=" + finalSymbol + ", buyOrder=" + buyOrder + ", sellOrder=" + sellOrder + '}';
+    }
+    
 }
